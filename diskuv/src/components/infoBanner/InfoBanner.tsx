6ef843f5a9e24be6141032704117fc5a452dbdb1
hrach105 <hrachkhachatryan24@gmail.com>
2022-06-13 14:35:10 +0400

    adding info banner images

--- diskuv/src/components/infoBanner/InfoBanner.tsx
+++ diskuv/src/components/infoBanner/InfoBanner.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import BannerDesc from '../banner/BannerDesc'
 import Button from '../Button'
+import InfoBannerSvg1 from  '../../images/info-banner-square.svg'
+import InfoBannerSvg2 from  '../../images/info-banner-circle.svg'
 import './infoBanner.scss'
 
 
 const InfoBanner = () => {
   return (
     <div className='info-banner'>
+      <img src={InfoBannerSvg1} alt="" />
+      <img src={InfoBannerSvg2} alt="" />
       <div className="info-banner-content">
         <BannerDesc 
           bannerTitle='Your safety is our first priority' 
